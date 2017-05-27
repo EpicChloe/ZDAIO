79827b5b5419fba2a0f39299c817c3c4a34d0949
Chris Pittelko <elda.theelf@gmail.com>
2017-05-27 06:53:38 -0700

    Updated README.md Added settings module to handle auth tokens (added to .gitignore) Changed layout so Mixpanel data is in separate tabs Cleaned up handlebar templates Added User Activity Modal Added Feedback to Actions Menu Resized text in Actions Menu Added Accordion Support Added Mixpanel User Activity to Modal Fixed issue with Notify Service interfering with other Bootstrap Modals Fixed Issue with app state persisting between tickets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ZDAIO
+# Ring ZenDesk All-In-One App
+
+## Requirements
+
+This requires the ZenDesk Application Tools, or ZAT, to be installed to package and locally host the ZenDesk app. For Ring Specific Data, a settings file must be created as /lib/settings.js returning the following payload:
+
+{ "MPAuth": "Basic " + Token }
--- app.js
+++ app.js
@@ -7,7 +7,11 @@
             lastFiveTicketArray: [],
             callArray: [],
             userData: [],
-            ticketCount: []
+            ticketCount: [],
+            MPUserProfile: [],
+            MPActivity: [],
+            appSettings: {},
+            activeDisplay: ''
         },
 
         events: {
@@ -17,12 +21,17 @@
             'fetchUserData.done': 'handleUserData',
 
             'mp_fetchUserViaEmail.done': 'parseMPUserProfile',
+            'mp_fetchUserViaEmail.fail': 'failureHandlerMPEmail',
             'mp_fetchUserActivityViaEmail.done': 'parseMPUserActivity',
+            'mp_fetchUserActivityViaEmail.fail': 'failureHandlerMPActivity',
 
             'click .toggle-app': 'toggleAppContainer',
             'click #RAIOGoToAdmin': 'RAIO_UI_ClickAdmin',
-            'click #RAIOMixPanelTabToggle': 'RAIO_UI_MixPanelActive',
-            'click #RAIOHomeTabToggle': 'RAIO_UI_HomeActive'
+            'click #RAIOMixPanelUserTabToggle': 'RAIO_UI_MixPanelActive',
+            'click .refresh-mp-userdata': 'RAIO_UI_MixPanelActive',
+            'click #RAIOHomeTabToggle': 'RAIO_UI_HomeActive',
+            'click #RAIOMixPanelActivityToggle': 'RAIO_UI_MixpanelActivity',
+
         },
 
         requests: {
@@ -30,16 +39,14 @@
                 return {
                     url: helpers.fmt("/api/v2/users/%@/tickets/requested.json?sort_order=desc", userID),
                     type: 'GET',
-                    dataType: 'json',
-                    proxy_v2: true
+                    dataType: 'json'
                 };
             },
             fetchUserData: function(id){
                 return {
                     url: helpers.fmt("/api/v2/users/%@.json?include=identities", id),
                     type: 'GET',
-                    dataType: 'json',
-                    proxy_v2: true
+                    dataType: 'json'
                 };
             },
             mp_fetchUserViaEmail: function(email){
@@ -50,7 +57,7 @@
                     dataType: 'json',
                     headers: {
                         "content-type": "application/x-www-form-urlencoded",
-                        "authorization": "Basic "
+                        "authorization": this.data.appSettings.MPAuth
                     },
                     data: {script: helpers.fmt('function main() { return People().filter(function(user){return user.properties.$email == "%@"})}', email)}
                 };
@@ -63,17 +70,24 @@
                     dataType: 'json',
                     headers: {
                         "content-type": "application/x-www-form-urlencoded",
-                        "authorization": "Basic "
+                        "authorization": this.data.appSettings.MPAuth
                     },
                     data: {script: helpers.fmt('function main() {return Events({from_date: "' + moment().subtract(3, 'days').format("YYYY-M-D") + '",to_date: "' + moment().format("YYYY-M-D") + '"}).filter(function(event) {return event.distinct_id == "%@";});}', email)}
                 };
             }
         },
 
-        init: function() {
+        init: function(data) {
             var self = this;
 
+            if (!data.firstLoad) {
+                self.RAIO_UI_Reset();
+            }
+
             var requesterId = self.ticket().requester().id();
+
+            self.data.appSettings = require('settings');
+
             self.ajax('fetchUserData', requesterId);
             self.ajax('fetchUserHistory', requesterId);
 
@@ -103,12 +117,8 @@
             var self = this;
 
             self.data.email = data.user.email;
-            console.log(data);
 
             self.data.userData = data.user;
-
-            self.ajax( 'mp_fetchUserViaEmail', self.data.email);
-            self.ajax( 'mp_fetchUserActivityViaEmail', self.data.email);
 
             self.data.validEmail = self.validateEmail(self.data.email);
 
@@ -164,15 +174,92 @@
         },
 
         parseMPUserProfile: function(data) {
+            var self = this;
+
             console.log('-- Mixpanel User Profile --');
             console.log(data);
             console.log('----------');
+
+            self.data.MPUserProfile = [];
+
+            if (data[0].properties['$ios_app_version'] != null) {
+                self.data.MPUserProfile.push({key:'iOS Device', value:data[0].properties['$ios_device_model']});
+                self.data.MPUserProfile.push({key:'App Version', value:data[0].properties['$ios_app_version']});
+            }
+
+            if (data[0].properties['$android_app_version'] != null) {
+                self.data.MPUserProfile.push({key:'Android Model', value:data[0].properties['$android_model'], tooltip:data[0].properties['$android_manufacturer']});
+                self.data.MPUserProfile.push({key:'Android OS', value:data[0].properties['$android_os_version']});
+                self.data.MPUserProfile.push({key:'App Version', value:data[0].properties['$android_app_version']});
+            }
+
+            self.data.MPUserProfile.push({key:'Allowing Location', value:data[0].properties['Allowing Location']});
+            self.data.MPUserProfile.push({key:'Allowing Microphone', value:data[0].properties['Allowing Microphone']});
+            self.data.MPUserProfile.push({key:'Allowing Notifications', value:data[0].properties['Allowing Notifications']});
+            self.data.MPUserProfile.push({key:'Latest Setup Device Type', value:data[0].properties['Latest Setup Device Type']});
+            self.data.MPUserProfile.push({key:'Latest Setup Result', value:data[0].properties['Latest Setup Result']});
+
+            self.$('#mp-user-refresh-icon').removeClass('spin');
+            self.data.activeDisplay = 'userProfile';
+            self.displayUpdate();
         },
 
         parseMPUserActivity: function(data) {
+            var self = this;
+
             console.log('-- Mixpanel User Activity --');
             console.log(data);
             console.log('----------');
+
+            self.data.MPActivity = [];
+
+            var lastTenEvents = _.first(data, 10).sort(function(a,b) {
+                var aID = a.time;
+                var bID = b.time;
+                return (aID === bID) ? 0 : (aID < bID) ? 1 : -1;
+            });
+
+            _.each(lastTenEvents, function(data) {
+                self.data.MPActivity.push({
+                    title:data.name,
+                    dataSet:[
+                        {'key': 'Event Type', 'value': data.properties['Event Type']},
+                        {'key': 'OS', 'value': data.properties.$os + ': ' + data.properties.$os_version},
+                        {'key': 'App Version', 'value': data.properties.$app_version},
+                        {'key': 'Model Phone', 'value': data.properties.$manufacturer + ' ' + data.properties.$app_version},
+                        {'key': 'Carrier', 'value': data.properties.$carrier},
+                        {'key': 'Radio', 'value': data.properties.$radio},
+                        {'key': 'Wifi', 'value': data.properties.$wifi},
+                        {'key': 'Allowing Location', 'value': data.properties['Allowing Location']},
+                        {'key': 'Allowing Microphone', 'value': data.properties['Allowing Microphone']},
+                        {'key': 'Allowing Notifications', 'value': data.properties['Allowing Notifications']}
+                    ]
+                });
+            }.bind(self));
+
+            console.log(self.data.MPActivity);
+
+            self.displayUpdate('userActivity');
+        },
+
+        failureHandlerMPEmail: function() {
+            var self = this;
+
+            services.notify('Error in Fetching Additional User Data. Retrying in 5 seconds.', 'error');
+
+            setTimeout(function() {
+                self.ajax('mp_fetchUserViaEmail', self.data.email);
+            }.bind(self), 5000);
+        },
+
+        failureHandlerMPActivity: function() {
+            var self = this;
+
+            services.notify('Error in Fetching Additional User Data. Retrying in 5 seconds.', 'error');
+
+            setTimeout(function() {
+                self.ajax( 'mp_fetchUserActivityViaEmail', self.data.email);
+            }.bind(self), 5000);
         },
 
         toggleAppContainer: function(){
@@ -191,7 +278,7 @@
             }
         },
 
-        displayUpdate: function() {
+        displayUpdate: function(tab) {
             var self = this,
                 templatePayload = {};
 
@@ -209,10 +296,44 @@
             templatePayload.tickets = self.data.ticketCount;
             templatePayload.user = self.data.userData;
             templatePayload.validEmail = self.data.validEmail;
+            templatePayload.MPUserProfile = self.data.MPUserProfile;
+            templatePayload.MPActivity = self.data.MPActivity;
 
-            console.log(templatePayload);
+            //console.log(templatePayload);
             self.switchTo('main', templatePayload);
 
+            if(self.data.activeDisplay === 'userProfile') {
+                this.$('.tab').addClass('hidden');
+                this.$('#RAIOMixpanelUserTab').removeClass('hidden');
+                this.$('#RAIOMixPanelUserTabToggle').parent().addClass('hidden');
+                this.$('#RAIOHomeTabToggle').parent().removeClass('hidden');
+            }
+
+            if(tab === 'userActivity') {
+                this.$('.jGrowl-notification').html('');
+                this.$('#RAIOMixpanelActivityModal').modal('show');
+            }
+
+            // Accordion Support
+            self.$(".set > a").on("click", function(e){
+                if(self.$(this).hasClass('active')){
+                    self.$(this).removeClass("active");
+                    self.$(this).siblings('.content').slideUp(200);
+                }else{
+                    self.$(".set > a").removeClass("active");
+                    self.$(this).addClass("active");
+                    self.$('.content').slideUp(200);
+                    self.$(this).siblings('.content').slideDown(200);
+                }
+                (e).preventDefault();
+            });
+
+        },
+
+        RAIO_UI_Reset: function() {
+            var self = this;
+            self.data.activeDisplay = '';
+            self.displayUpdate();
         },
 
         RAIO_UI_ClickAdmin: function(event) {
@@ -226,11 +347,26 @@
         },
 
         RAIO_UI_MixPanelActive: function(event) {
+            var self = this;
             event.preventDefault();
             this.$('.tab').addClass('hidden');
-            this.$('#RAIOMixpanelTab').removeClass('hidden');
-            this.$('#RAIOMixPanelTabToggle').parent().addClass('hidden');
+            this.$('#RAIOMixpanelUserTab').removeClass('hidden');
+            this.$('#RAIOMixPanelUserTabToggle').parent().addClass('hidden');
             this.$('#RAIOHomeTabToggle').parent().removeClass('hidden');
+
+                self.ajax('mp_fetchUserViaEmail', self.data.email);
+                self.$('#mp-user-refresh-icon').addClass('spin');
+
+        },
+
+        RAIO_UI_MixpanelActivity: function(event) {
+            var self = this;
+
+            event.preventDefault();
+
+            services.notify('Fetching User Activity...','notice');
+
+            self.ajax( 'mp_fetchUserActivityViaEmail', self.data.email);
         },
 
         RAIO_UI_HomeActive: function(event) {
@@ -238,9 +374,9 @@
             this.$('.tab').addClass('hidden');
             this.$('#RAIOHomeTab').removeClass('hidden');
             this.$('#RAIOHomeTabToggle').parent().addClass('hidden');
-            this.$('#RAIOMixPanelTabToggle').parent().removeClass('hidden');
+            this.$('#RAIOMixPanelUserTabToggle').parent().removeClass('hidden');
+            self.data.activeDisplay = '';
         },
-
 
         validateEmail: function (email) {
 
@@ -248,7 +384,7 @@
             var validation1 = /^[^@\s]+@talkdesk.com$/i.test(email),
                 validation2 = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/.test(email);
 
-            console.log("Email Validation1: "+validation1+" Email Validation2: "+validation2);
+            //console.log("Email Validation1: "+validation1+" Email Validation2: "+validation2);
 
             return (!validation1 && validation2);
         }
